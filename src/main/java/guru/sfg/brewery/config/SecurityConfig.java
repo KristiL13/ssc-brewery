248ec59c113aa3bd97061c3153e5ef837f3dbf74
Kristi Laur <kristi@heisi.eu>
2021-12-22 15:50:52 +0200

    Add comments

--- src/main/java/guru/sfg/brewery/config/SecurityConfig.java
+++ src/main/java/guru/sfg/brewery/config/SecurityConfig.java
@@ -14,12 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // osa sellest sisust on pärit super klassist
                 http
-                .authorizeRequests(authorize -> {
+                .authorizeRequests(authorize -> { // index leht ja erinevad fronti .js jmt koodi kaustad, et saaks kuvada lehte
+                    // vaatas brauseris Network alt, et millistele staatilistele ressurssidele ei p22se ligi
                     authorize.antMatchers("/", "/webjars/**", "/login", "/resources/**").permitAll();
                 } )
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated() // kõik erandid peavad olema toodud yleval pool seda rida
                 .and()
                 .formLogin().and()
                 .httpBasic();
